0bdbd1f199e963a6967316d95c9ecc919b1234dd
Slade Galbraith <sladegalbraith@Slades-MacBook-Air.local>
2026-02-18 12:40:46 +1000

    Remove SQLite connect_args from repository to fix Postgres DSN error

--- tenancy/repository.py
+++ tenancy/repository.py
@@ -1,29 +1,13 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from tenancy.models import Base
-
 from sqlalchemy.orm import Session
-from database import DATABASE_URL
-from tenancy.models import Organisation
 from database import SessionLocal
+from tenancy.models import Organisation
 
-engine = create_engine(
-   DATABASE_URL,
-   connect_args={"check_same_thread": False}
-)
-
-SessionLocal = sessionmaker(
-   autocommit=False,
-   autoflush=False,
-   bind=engine
-)
-
-def init_tenancy_db():
-   Base.metadata.create_all(bind=engine)
 
 def get_tenant(tenant_id: str):
    db: Session = SessionLocal()
    try:
-      return db.query(Organisation).filter(Organisation.id == tenant_id).first()
+       return db.query(Organisation).filter(
+           Organisation.id == tenant_id
+       ).first()
    finally:
-      db.close()
+       db.close()
